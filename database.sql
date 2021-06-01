993630a28b92f8ff4f28c2b299ee5522671d1b50
rambaldi <diencong5998@gmail.com>
2021-06-01 15:30:54 +0700

    database new update now

--- database.sql
+++ database.sql
@@ -27,9 +27,40 @@ create table student(
                         dateOfBirth date,
                         identity varchar(20),
                         address varchar(200),
-                        status varchar(20)
+                        status varchar(200),
+                        class_id int not null ,
+                        foreign key class_id (class_id) references class(id)
 );
 
+
+create table diary_class(
+    class_id int not null ,
+    teacher_id int not null ,
+    date date ,
+    diary text,
+    foreign key class_id (class_id) references class(id),
+    foreign key teacher_id (teacher_id) references teacher(id)
+);
+create table diary_student(
+                            student_id int not null ,
+                            teacher_id int not null ,
+                            date date ,
+                            diary text,
+                            foreign key student_id (student_id) references student(id),
+                            foreign key teacher_id (teacher_id) references teacher(id)
+);
+
+create table subject(
+    id int primary key auto_increment,
+    name nvarchar(200) not null
+);
+
+create table point (
+    subject_id int ,
+    teacher_id int,
+    student_id int,
+    point float
+);
 
 create table role (
                       id int primary key auto_increment,
