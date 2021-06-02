d2c14fba8244deb649ecab238941cc68dc6df431
duyhai97 <79962575+duyhai97@users.noreply.github.com>
2021-06-02 16:02:05 +0700

    update package model

--- database.sql
+++ database.sql
@@ -1,23 +1,74 @@
+create database manager_teaching_center;
+use manager_teaching_center;
 
-response.setContentType("text/html; charset= UTF-8");
-        PrintWriter out = response.getWriter();
-
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        boolean isValid = AccountJDBC.checkLogin(username,password);
-
-        if (isValid){
-            HttpSession session = request.getSession();
-
-session.setAttribute("tendangnhap", username);
-            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("index.jsp");
-            dispatcher.forward(request,response);
-
+create table student(
+                        id int not null auto_increment primary key ,
+                        name varchar(100) ,
+                        email varchar(100) unique ,
+                        identify varchar(100),
+                        dateOfBirth date,
+                        address varchar(100),
+                        phoneNumber varchar(11),
+                        id_class int,
+                        status nvarchar(250),
+                        foreign key (id_class) references class(id)
+);
 
 
-}
-        else {
-            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/login.jsp");
-}
+create table teacher(
+                        id int not null auto_increment primary key ,
+                        name varchar(100),
+                        dateOfBirth date,
+                        address varchar(100),
+                        phoneNumber varchar(11),
+                        description varchar(250),
+                        email varchar(100) unique
+);
 
-        out.close();
+create table class(
+                      id int not null auto_increment primary key ,
+                      name varchar(100) unique
+);
+
+
+create table subject (
+                         id int not null auto_increment primary key ,
+                         name varchar(100) unique
+);
+
+create  table point (
+                        id_subject int ,
+                        id_teacher int,
+                        id_student int,
+                        point int,
+                        foreign key (id_subject) references subject(id),
+                        foreign key (id_teacher) references teacher(id),
+                        foreign key (id_student) references student(id)
+);
+
+create table diary_class(
+                            id_class int,
+                            id_teacher int,
+                            date date,
+                            diary text,
+                            foreign key (id_class ) references class(id),
+                            foreign key (id_teacher) references teacher(id)
+);
+
+create table diary_student(
+                              id_student int,
+                              id_teacher int,
+                              date date,
+                              diary text,
+                              foreign key (id_student) references student(id),
+                              foreign key (id_teacher) references teacher(id)
+);
+create table role(
+                     id int not null auto_increment primary key ,
+                     name varchar(100)
+);
+
+create table admin(
+                      username varchar(100) not null primary key ,
+                      password varchar(100) not null
+);
